53ee2f59dc9372b2e8154db16f2c937398a196cc
BaconLover307 <kresnadi30@gmail.com>
2019-10-24 16:22:29 +0700

    Latest Update

---
+++ src/tes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "assets.c"
+
+void main()
+{
+    int a = 10, b = 20;
+    int sum = 0;
+    printf("Tes");
+    clrscrn();
+    sum = a + b;
+    printf("Sum: %d", sum);
+    getchar();
+}
