4a26413d6fb3c07b0ce3228f583e757d5dcd791b
annaelvira24 <52845049+annaelvira24@users.noreply.github.com>
2019-10-26 01:00:17 +0700

    upload driver queue, udh bisa add/del + tulis head

---
+++ src/driver_queue.c
@@ -0,0 +1,36 @@
+#include "player.h"
+#include "pcolor.h"
+#include "string.h"
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// $ Kamus
+Player P1,P2;
+char color1,color2;
+
+int main(){
+    CreatePlayer(&P1);
+
+    char skil [10];
+    char temp [10];
+
+    PrintInfoHead(Skill(P1));
+    getchar();
+
+    QAdd(&Skill(P1), "B");
+    PrintInfoHead(Skill(P1));
+    getchar();
+
+    QDel(&Skill(P1), &temp);
+    PrintInfoHead(Skill(P1));
+    getchar();
+
+    QDel(&Skill(P1), &temp);
+    PrintInfoHead(Skill(P1));
+    getchar();
+
+
+
+    
+}
