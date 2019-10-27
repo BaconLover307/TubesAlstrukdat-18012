4629a35eb04742393f9d3b47ba781e5c6e44e9d9
Daniel-Ri <danielriyanto190700@gmail.com>
2019-10-27 14:31:53 +0700

    Menambahkan beberapa algoritma pada ADT Graph

--- src/graph.c
+++ src/graph.c
@@ -5,67 +5,86 @@
 /* 4. Faris Muhammad Kautsar   / 13518105 */
 /* 5. Gregorius Jovan Kresnadi / 13518135 */
 
-/* File header: graph.h */
+/* File body : graph.h */
 
-#ifndef GRAPH_H
-#define GRAPH_H
-
-#include "boolean.h"
-#include "listlinier.h"
-
-typedef struct {
-  List *GI;  /* memori tempat penyimpan elemen (container) */
-  int NeffG;   /* >=0, banyaknya elemen efektif */
-  int MaxG;  /* ukuran elemen */
-} Graph;
-
-/* Indeks yang digunakan [IdxMin..MaxG] */
-// * IdxMin sudah didefinisi pada ADT Array
-/* Jika G adalah Graph, cara deklarasi dan akses: */
-/* Deklarasi : G : Graph */
-/* Maka cara akses:
-   G.GI     untuk mengakses seluruh List pada Graph G
-   G.GI[i]  untuk mengakses List ke-i 
-   G.NeffG  untuk mengetahui banyaknya elemen
-   G.MaxG   untuk mengetahui batas jumlah elemen Graph */    
-
-/* Definisi :
-  Graph kosong : G.NeffG = 0
-  Definisi elemen pertama : G.GI[i] dengan i=1
-  Definisi elemen terakhir yang terdefinisi: G.GI[i] dengan i=G.NeffG */
-
-/* ********** SELEKTOR ********** */
-#define NeffG(G) (G).NeffG
-#define GI(G) (G).GI
-#define ElmtG(G, i) (G).GI[(i)]
-#define MaxG(G) (G).MaxG
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "graph.h"
 
 /* ********** KONSTRUKTOR ********** */
 /* Konstruktor : create Graph kosong  */
-void MakeEmptyGraph (Graph * G, int maxel);
+void MakeEmptyGraph (Graph * G, int maxel) {
 /* I.S. G sembarang, maxel > 0 */
 /* F.S. Terbentuk tabel G kosong dengan kapasitas maxel + 1 */
 
-void DealokasiGraph (Graph * G);
+  /* KAMUS LOKAL */
+  int i;
+
+  /* ALGORITMA */
+  GI(*G) = (List *) malloc ((maxel + 1)* sizeof(List));
+  MaxG(*G) = maxel;
+  NeffG(*G) = 0;
+
+  for (i = IdxMin; i <= maxel; i++) {
+    CreateEmptyList(&ElmtG(*G, i));
+  }
+}
+
+void DealokasiGraph (Graph * G) {
 /* I.S. G terdefinisi; */
 /* F.S. GI(G) dikembalikan ke system, MaxG(G)=0; NeffG(G)=0 */
 
+  /* KAMUS LOKAL */
+
+  /* ALGORITMA */
+  free(GI(*G));
+  MaxG(*G) = 0;
+  NeffG(*G) = 0;
+}
+
 /* *** Selektor INDEKS *** */
-IdxType GetFirstGraph (Graph G);
+IdxType GetFirstGraph (Graph G) {
 /* Prekondisi : Graph G tidak kosong */
 /* Mengirimkan indeks elemen G pertama */
-IdxType GetLastGraph (Graph G);
+
+  /* KAMUS LOKAL */
+
+  /* ALGORITMA */
+  return IdxMin;
+}
+
+IdxType GetLastGraph (Graph G) {
 /* Prekondisi : Graph G tidak kosong */
 /* Mengirimkan indeks elemen G terakhir */
 
+  /* KAMUS LOKAL */
+
+  /* ALGORITMA */
+  return NeffG(G);
+}
+
 /* ********** TEST KOSONG/PENUH ********** */
 /* *** Test tabel kosong *** */
-boolean IsEmptyGraph (Graph G);
+boolean IsEmptyGraph (Graph G) {
 /* Mengirimkan true jika Graph G kosong, mengirimkan false jika tidak */
+
+  /* KAMUS LOKAL */
+
+  /* ALGORITMA */
+  return (NeffG(G) == 0);
+}
+
 /* *** Test tabel penuh *** */
-boolean IsFullGraph (Graph G);
+boolean IsFullGraph (Graph G) {
 // ? Entah perlu fungsi ini gak ya?  
 /* Mengirimkan true jika Graph G penuh, mengirimkan false jika tidak */
+
+  /* KAMUS LOKAL */
+
+  /* ALGORITMA */
+  return (NeffG(G) == MaxG(G));
+}
 
 /* ********** KELOMPOK BACA/TULIS ********** */
 void BacaGraph (Graph * G);
@@ -74,11 +93,17 @@ void BacaGraph (Graph * G);
 /* I.S. Graph G terdefinisi */
 /* F.S. Graph G berisi hubungan-hubungan antar bangunan */
 
-void TambahRelation (Graph * G, urutan X, urutan Y);
+void TambahRelation (Graph * G, urutan X, urutan Y) {
 /* I.S. Graph G terdefinisi */
 /* F.S. Bangunan ke-X dinyatakan memiliki hubungan dengan bangunan ke-Y */
 
-void TulisGraph (Graph G);
+  /* KAMUS LOKAL */
+
+  /* ALGORITMA */
+  InsVPrio(&ElmtG(*G, X), Y);
+}
+
+void TulisGraph (Graph G) {
 // ? Untuk debugging doang kayaknya 
 /* I.S. G terdefinisi */
 /* F.S. Hubungan antar bangunan ditampilkan ke layar. 
@@ -90,11 +115,42 @@ void TulisGraph (Graph G);
 1 0 0
 */
 
+  /* KAMUS LOKAL */
+  IdxType i, j;
+
+  /* ALGORITMA */
+  for (i = IdxMin; i <= NeffG(G); i++) {
+    for (j = IdxMin; j < NeffG(G); j++) {
+      if (CheckRelation(G, i, j)) {
+        printf("0 ");
+      } else /* !CheckRelation(G, i, j) */ {
+        printf("1 ");  
+      }
+    }
+
+    if (CheckRelation(G, i, NeffG(G))) {
+      printf("0\n");
+    } else /* !CheckRelation(G, i, NeffG(G)) */ {
+      printf("1\n");  
+    }
+  }
+}
+
 /******************* RELATION **********************/
-boolean CheckRelation (Graph G, urutan X, urutan Y);
+boolean CheckRelation (Graph G, urutan X, urutan Y) {
 /* Mengecek apakah bangunan ke-X memiliki hubungan dengan bangunan ke-Y 
    atau tidak */
 /* Jika iya maka True dan sebaliknya */
+
+  /* KAMUS LOKAL */
+
+  /* ALGORITMA */
+  if ((Search(ElmtG(G, X), Y)) != Nil) {
+    return true;
+  } else /* Search(ElmtG(G, X), Y)) == Nil */ {
+    return false;
+  }
+}
 
 /******************* ATTACK *********************/
 boolean CheckAttack (Graph G, List L, urutan X);
@@ -126,5 +182,3 @@ void PrintMove (Graph G, List L, urutan X);
         yang dapat ditambahkan jumlah tentaranya.
         Jika tidak ada maka muncul "Tidak ada bangunan terdekat" di 
         layar */
-
-#endif  
