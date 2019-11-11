d3ed6673728d8c2bbe5d898d6105691415f84387
Daniel-Ri <danielriyanto190700@gmail.com>
2019-11-11 12:08:51 +0700

    Mengubah file-file yang tidak ada nama "daniel" dengan file-file yang ada "daniel" dan menghapus file-file yang ada nama "daniel"

--- src/array.h
+++ src/array.h
@@ -14,67 +14,59 @@
 #include "point.h"
 
 /*  Kamus Umum */
-#define IdxMax 29
-/* Indeks maksimum array */
 #define IdxMin 1
 /* Indeks minimum array */
-#define CharUndef 'Z'
-/* Nama Bangunan yang tidak terdefinisi */
-#define ValUndef 0 // ! Untuk Relate (sementara)
-/* Nilai elemen tak terdefinisi */
 
 /* Definisi elemen dan koleksi objek */
 typedef int IdxType;  /* type indeks */
 typedef int ElType;   /* type elemen tabel */ // ! Untuk Relate (sementara)
 
 typedef struct {
-  ElType RI[IdxMax + 1]; /* memori tempat penyimpanan elemen (container) */
-} RELATE; /* class yang menyimpan dengan hubungan bangunan ke-berapa aja */
-// ! Ini untuk sementara doang
-
-typedef struct {
   char name; /* tipe bangunan */
   POINT posisi; /* posisi bangunan */
   int level; /* level bangunan */
-  RELATE hubungan; /* hubungan dengan bangunan ke-berapa */ // ! Sementara doang
   int tentara; /* jumlah tentara yang dimiliki bangunan */
 } info_bangunan;
 
 typedef struct {
-  info_bangunan BI[IdxMax+1]; /* memori tempat penyimpan elemen (container) */
+  info_bangunan *BI; /* memori tempat penyimpan elemen (container) */
+  int Neff;   /* >=0, banyaknya elemen efektif */
+  int MaxEl;  /* ukuran elemen */
 } Bangunan;
-/* Indeks yang digunakan [IdxMin..IdxMax] */
+/* Indeks yang digunakan [IdxMin..MaxEl] */
 /* Jika B adalah Bangunan, cara deklarasi dan akses: */
 /* Deklarasi : B : Bangunan */
 /* Maka cara akses:
+   B.Neff  untuk mengetahui banyaknya elemen
    B.BI    untuk mengakses seluruh nilai elemen tabel
    B.BI[i] untuk mengakses elemen ke-i */
 /* Definisi :
+   Tabel kosong: B.Neff = 0
    Definisi elemen pertama : B.BI[i] dengan i=1
-   Definisi elemen terakhir yang terdefinisi: T.TI[i] dengan i=T.Neff */
+   Definisi elemen terakhir yang terdefinisi: B.BI[i] dengan i=B.Neff */
 
 /* ********** SELEKTOR ********** */
 /* B adalah Bangunan      */
 /* e adalah info_bangunan */
-/* R adalah Relate        */
+#define Neff(B)       (B).Neff
 #define BI(B)         (B).BI
 #define ElmtBan(B,i)  (B).BI[(i)]
+#define MaxEl(B)      (B).MaxEl
 
 #define Name(e)       (e).name
 #define Posisi(e)     (e).posisi
 #define Level(e)      (e).level
-#define Relate(e)     (e).hubungan
 #define Tentara(e)    (e).tentara
-
-#define RI(R)         (R).RI
-#define ElmtRel(R,i)  (R).RI[(i)]
 
 /* ********** KONSTRUKTOR ********** */
 /* Konstruktor : create tabel kosong  */
-void MakeEmptyBangunan (Bangunan * B);
-/* I.S. B sembarang */
-/* F.S. Terbentuk tabel B kosong dengan kapasitas IdxMax-IdxMin+1 */
-/* Proses: Inisialisasi semua nama elemen tabel B dengan charUndef */
+void MakeEmptyBangunan (Bangunan * B, int maxel);
+/* I.S. B sembarang, maxel > 0 */
+/* F.S. Terbentuk tabel B kosong dengan kapasitas maxel + 1 */
+
+void DealokasiBangunan(Bangunan *B);
+/* I.S. B terdefinisi; */
+/* F.S. BI(B) dikembalikan ke system, MaxEl(B)=0; Neff(B)=0 */
 
 /* ********** SELEKTOR (TAMBAHAN) ********** */
 /* *** Banyaknya elemen *** */
@@ -85,6 +77,7 @@ int NbElmtBan (Bangunan B);
 IdxType GetFirstBan (Bangunan B);
 /* Prekondisi : Tabel B tidak kosong */
 /* Mengirimkan indeks elemen B pertama */
+
 IdxType GetLastBan (Bangunan B);
 /* Prekondisi : Tabel B tidak kosong */
 /* Mengirimkan indeks elemen B terakhir */
@@ -93,9 +86,10 @@ IdxType GetLastBan (Bangunan B);
 /* *** Test tabel kosong *** */
 boolean IsEmptyBan (Bangunan B);
 /* Mengirimkan true jika tabel B kosong, mengirimkan false jika tidak */
+
 /* *** Test tabel penuh *** */
 boolean IsFullBan (Bangunan B);
-// ? Entah perlu fungsi ini gak ya?  
+// ? Entah perlu fungsi ini gak ya?
 /* Mengirimkan true jika tabel B penuh, mengirimkan false jika tidak */
 
 /* ********** BACA dan TULIS dengan INPUT/OUTPUT device ********** */
@@ -103,8 +97,8 @@ boolean IsFullBan (Bangunan B);
 void BacaBangunan (Bangunan * B);
 /* I.S. B sembarang */
 /* F.S. Tabel B terdefinisi */
+/* Prekondisi : Banyaknya elemen B itu valid */
 /* Proses : membaca banyaknya elemen B dan mengisi semua nilainya dengan Mesin Kata */
-// Todo Menunggu yang kerjain Configure 
 
 /*********************** Tentara Attack ****************************/
 boolean CheckAttackTentara (Bangunan B, IdxType X, int N);
@@ -113,15 +107,23 @@ boolean CheckAttackTentara (Bangunan B, IdxType X, int N);
 
 void TentaraAttack (Bangunan * B, IdxType X, int N);
 /* I.S. Bangunan B terdefinisi
-        X pasti ada di dalam List 
+        X pasti ada di dalam List
         Jumlah N pasti normal (sudah dicek pakai CheckAttackTentara) */
-/* F.S. Bangunan ke-X mengalami pengurangan jumlah tentara sebesar N */        
+/* F.S. Bangunan ke-X mengalami pengurangan jumlah tentara sebesar N */
 
 /*********************** Tentara Invaded **************************/
-void TentaraInvaded (Bangunan * B, IdxType i, int N);
-/* I.S. Bangunan B terdefinisi 
-        i pasti ada di dalam indeks bangunan B */
-/* F.S. Bangunan ke-i mengalami penurunan jumlah tentara sebesar N (mungkin minus) */
+void InvadedShield (Bangunan * B, IdxType i, int N);
+/* I.S. Bangunan B terdefinisi
+        i pasti ada di dalam indeks bangunan B
+        Skill Shield itu aktif sekali          */
+/* F.S. Bangunan ke-i mengalami pengurangan jumlah tentara (mungkin minus)
+        dengan perhitungan adanya pertahanan */
+
+void TentaraInvaded (Bangunan * B, boolean Critical_Hit, boolean Attack_Up, int Shield, IdxType i, int N);
+/* I.S. Bangunan B terdefinisi
+        i pasti ada di dalam indeks bangunan B
+        Mengetahui skill-skill tertentu aktif atau tidak*/
+/* F.S. Bangunan ke-i mengalami pengurangan jumlah tentara (mungkin minus) */
 
 boolean CanCapture (Bangunan B, IdxType i);
 /* Mengecek apakah Bangunan ke-i memiliki jumlah tentara yang <= 0 */
@@ -135,7 +137,7 @@ void TentaraAbsolute (Bangunan * B, IdxType X);
 
 /*************************** Level-Up Bangunan ********************************/
 boolean CheckLevelUp (Bangunan B, IdxType X);
-/* Mengecek apakah bangunan tertentu sudah melewati jumlah yang seharusnya 
+/* Mengecek apakah bangunan tertentu sudah melewati jumlah yang seharusnya
    atau belum */
 /* Jika iya maka True dan sebaliknya */
 // ! Jika udah level 4 maka tidak bisa Level Up lagi
@@ -143,7 +145,7 @@ boolean CheckLevelUp (Bangunan B, IdxType X);
 void LevelUp (Bangunan * B, IdxType X);
 /* I.S. Bangunan B terdefinisi
         Bangunan ke-X sudah pasti bisa Level-Up */
-/* F.S. Bangunan ke-X mengalami pengurangan jumlah tentara sebesar 1/2 dari 
+/* F.S. Bangunan ke-X mengalami pengurangan jumlah tentara sebesar 1/2 dari
         jumlah sebelumnya.
         Bangunan ke-X menglami penaikan 1 level. */
 
