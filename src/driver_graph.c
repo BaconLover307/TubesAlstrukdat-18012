b8f6a2012cb7ae2679ae451af3811eb102464544
Daniel-Ri <danielriyanto190700@gmail.com>
2019-11-20 07:30:12 +0700

    Menambahkan testing tambahan untuk driver_graph

--- src/driver_graph.c
+++ src/driver_graph.c
@@ -16,15 +16,76 @@ int main() {
   /* KAMUS */
   Graph G;
   List L1, L2;
+  Bangunan B;
+  int N, X, Y;
 
   /* ALGORITMA */
   MakeEmptyGraph(&G);
-  AddParent(&G, 5);
-  AddRelation(&G, 1, 3);
-  AddRelation(&G, 3, 1);
-  AddRelation(&G, 2, 5);
-  AddRelation(&G, 5, 2);
+
+  N = 5;
+  AddParent(&G, N);
+
+  X = 1; Y = 3;
+  AddRelation(&G, X, Y);
+  X = 3; Y = 1;
+  AddRelation(&G, X, Y);
+  X = 2; Y = 5;
+  AddRelation(&G, X, Y);
+  X = 5; Y = 2;
+  AddRelation(&G, X, Y);
+  X = 1; Y = 5;
+  AddRelation(&G, X, Y);
+  X = 5; Y = 1;
+  AddRelation(&G, X, Y);
+  X = 2; Y = 4;
+  AddRelation(&G, X, Y);
+  X = 4; Y = 2;
+  AddRelation(&G, X, Y);
+
   TulisGraph(G);
+
+  CreateEmptyList(&L1); CreateEmptyList(&L2);
+  MakeEmptyBangunan(&B, 5);
+  Neff(B) = 5;
+
+  Name(ElmtBan(B, 1)) = 'C';
+  X = 10; Y = 1; MakePOINT(&Posisi(ElmtBan(B, 1)), X, Y);
+  Level(ElmtBan(B, 1)) = 1;
+  Tentara(ElmtBan(B, 1)) = 40;
+
+  Name(ElmtBan(B, 2)) = 'C';
+  X = 1; Y = 15; MakePOINT(&Posisi(ElmtBan(B, 2)), X, Y);
+  Level(ElmtBan(B, 2)) = 1;
+  Tentara(ElmtBan(B, 2)) = 40;
+
+  Name(ElmtBan(B, 3)) = 'V';
+  X = 1; Y = 9; MakePOINT(&Posisi(ElmtBan(B, 3)), X, Y);
+  Level(ElmtBan(B, 3)) = 1;
+  Tentara(ElmtBan(B, 3)) = 20;
+
+  Name(ElmtBan(B, 4)) = 'T';
+  X = 1; Y = 13; MakePOINT(&Posisi(ElmtBan(B, 4)), X, Y);
+  Level(ElmtBan(B, 4)) = 1;
+  Tentara(ElmtBan(B, 4)) = 30;
+
+  Name(ElmtBan(B, 5)) = 'C';
+  X = 2; Y = 3; MakePOINT(&Posisi(ElmtBan(B, 5)), X, Y);
+  Level(ElmtBan(B, 5)) = 1;
+  Tentara(ElmtBan(B, 5)) = 40;
+
+  InsVPrio(&L1, 1); InsVPrio(&L1, 3);
+  InsVPrio(&L2, 2); InsVPrio(&L2, 4);
+
+  X = 1;
+  if (CheckAttack(G, L1, X)) {
+    printf("Bangunan ke-1 dapat menyerang lawan.\n");
+  }
+
+  printf("Daftar bangunan yang dapat diserang oleh bangunan ke-1:\n");
+  PrintAttack(G, L1, B, X);
+
+  printf("Daftar bangunan terdekat dengan bangunan ke-1:\n");
+  PrintMove(G, L1, B, X);
 
   return 0;
 }
